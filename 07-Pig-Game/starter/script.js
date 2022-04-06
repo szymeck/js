2dfe32c4b73fb76ee4d2b404b99ab161bb8d7231
szymeck <szymeck.sobierajski@gmail.com>
2022-04-06 14:06:42 +0200

    zmianyrobota3

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,14 +9,26 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0,0];
 let currentScore = 0;
+let activePlayer = 0;
 
 let diceRoll = Math.trunc(Math.random()*6)+1;
 
+const switchPlayer = function(){document.getElementById(`current--${activePlayer}`).textContent=0;
+currentScore=0;
+activePlayer= activePlayer===0 ? 1 :0;
+player0El.classList.toggle('player--active');
+player1El.classList.toggle('player--active');
+
+}
 
 
 btnRoll.addEventListener('click', function(){
@@ -28,10 +40,18 @@ diceEl.src = `dice-${dice}.png`;
 
 if(dice!==1){
 currentScore+=dice;
-current0El.textContent=currentScore;
+document.getElementById(`current--${activePlayer}`).textContent=currentScore;
 }
 else {
-
+switchPlayer();
 }
 
 });
+
+
+btnHold.addEventListener('click',function(){
+scores[activePlayer]+=currentScore;
+// scores[1]=scores[1]+currentScore;
+document.getElementById(`current--${activePlayer}`).textContent=scores[activePlayer];
+switchPlayer();
+})
