c281ec96b4db0430910cc03ea21735359cbb78e5
szymeck <szymeck.sobierajski@gmail.com>
2022-04-12 16:55:52 +0200

    zmianyrobota4

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,27 +12,37 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
+let scores,currentScore,activePlayer,playing;
+const init = function(){
+scores = [0,0];
+currentScore = 0;
+activePlayer = 0;
+playing =true;
+
+
+score1El.textContent=0;
+current0El.textContent=0;
+current1El.textContent=0;
+score0El.textContent=0;
+
 diceEl.classList.add('hidden');
-
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-
-let diceRoll = Math.trunc(Math.random()*6)+1;
-
+player0El.classList.remove('player--winner');
+player1El.classList.remove('player--winner');
+player0El.classList.add('player--active');
+player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function(){document.getElementById(`current--${activePlayer}`).textContent=0;
 currentScore=0;
 activePlayer= activePlayer===0 ? 1 :0;
 player0El.classList.toggle('player--active');
 player1El.classList.toggle('player--active');
 
+
 }
 
-
 btnRoll.addEventListener('click', function(){
-
+if(playing){
 const dice = Math.trunc(Math.random()*6)+1;
 console.log(dice);
 diceEl.classList.remove('hidden');
@@ -46,12 +56,23 @@ else {
 switchPlayer();
 }
 
-});
+}});
 
 
 btnHold.addEventListener('click',function(){
+    if(playing){
 scores[activePlayer]+=currentScore;
-// scores[1]=scores[1]+currentScore;
-document.getElementById(`current--${activePlayer}`).textContent=scores[activePlayer];
-switchPlayer();
-})
+document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
+if (scores[activePlayer]>=20){
+document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+playing=false;
+diceEl.classList.add('hidden');
+
+
+
+} else {
+switchPlayer();}
+}});
+
+btnNew.addEventListener('click',init);
