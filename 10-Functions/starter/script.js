857d65129322a1e4da0ee3c84f74ea38786212de
szymeck <szymek.sobierajski@gmail.com>
2022-09-08 17:44:05 +0200

    robota18

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,79 @@
 'use strict';
 
+
+
+
+
+
+
+
+const lufthansa = {
+    airline: 'lufthansa',
+    iataCode: 'lh',
+    bookings: [],
+    book(flightNum,name) { 
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+     this.bookings.push(`flight:${this.airline} ${this.iataCode}${flightNum}`)   
+    },
+}
+
+lufthansa.book(239,'szymi sob');
+lufthansa.book(635,'john sob');
+console.log(lufthansa);
+
+const book = lufthansa.book;
+
+const eurowings ={
+    airline:'eurowings',
+    iataCode:'ew',
+    bookings:[],
+};
+// not working
+// book(444,'sob sob');
+
+book.call(eurowings,23,'szym szym');
+
+// const greet = function(greeting) {
+//     return function(name){
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
+
+const greet =(greeting) => { return (name) => console.log(`${greeting} ${name}`)};
+
+
+
+const greetHey = greet (`hey`)
+greetHey('szymi');
+greetHey('sob');
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+    const [first,...others]= str.split(' ');
+    return [first.toUpperCase(),...others].join(' ');
+}
+
+// high order function
+const transformer = function(str,fn) {
+    console.log(`original string: ${str}`);
+    console.log(`trans string: ${fn(str)}`);
+    console.log(`trans by: ${fn.name}`);
+    
+}
+
+transformer('java is the best!',upperFirstWord);
+
+const high5 = function(){
+    console.log(`xd`);
+}
+document.body.addEventListener('click',high5);
+['jonas','szym',123].forEach(high5);
+
+
+
 let flight= 'lh123';
 
 const flight2 = {
@@ -10,6 +84,7 @@ const szymi = {
     name:'Szymon Sob',
     passport: 2121232323
 }
+
 
 const checkIn = function(flightNum,passenger){
 
@@ -25,9 +100,9 @@ const checkIn = function(flightNum,passenger){
     }
 }
 
-// checkIn(flight,szymi);
-// console.log(flight2);
-// console.log(szymi);
+checkIn(flight,szymi);
+console.log(flight2);
+console.log(szymi);
 
 const newPassport = function(person){
     person.passport=Math.trunc(Math.random()*100000000);
