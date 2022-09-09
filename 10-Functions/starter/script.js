c156d274d86b3165ad0d98b38ab40c63233856bd
szymeck <szymek.sobierajski@gmail.com>
2022-09-09 16:11:36 +0200

    robota19

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -13,7 +13,7 @@ const lufthansa = {
     bookings: [],
     book(flightNum,name) { 
         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
-     this.bookings.push(`flight:${this.airline} ${this.iataCode}${flightNum}`)   
+     this.bookings.push({flight:`${this.iataCode}${flightNum}`,name})   
     },
 }
 
@@ -31,18 +31,78 @@ const eurowings ={
 // not working
 // book(444,'sob sob');
 
+
+// call method
 book.call(eurowings,23,'szym szym');
+console.log(eurowings);
 
-// const greet = function(greeting) {
-//     return function(name){
-//         console.log(`${greeting} ${name}`);
-//     }
-// }
+const swiss = {
+    airline:'swiss',
+    iataCode:'sw',
+    bookings:[],
+};
+book.call(swiss,478,'holy sh');
 
-const greet =(greeting) => { return (name) => console.log(`${greeting} ${name}`)};
+console.log(swiss);
+
+// apply method
+const flightData = [853,'gorge george'];
+book.apply(swiss,flightData);
 
 
 
+
+
+
+// bind methtod
+
+const bookEw = book.bind(eurowings);
+const bookLh = book.bind(lufthansa);
+const bookSw = book.bind(swiss);
+bookEw(877,'elo elo');
+
+const bookEw23 = book.bind(eurowings,23);
+bookEw23('szymon sz');
+
+// with eventlsteners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function(){
+    console.log(this);
+
+    this.planes++;
+    console.log(this.planes);
+    console.log(this);
+};
+document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa));
+
+// partial method
+
+const addTax = (rate,value)=>value + value*rate;
+console.log(addTax(0.1,200));
+
+const addVat= addTax.bind(null,0.23,100)
+console.log(addVat());
+
+// challenge
+const addTaxRate= function(rate){
+    return function(value){
+        return value+value*rate;
+    }
+};
+const addTaxRate2=(rate)=>{return(value)=>value+value*rate};
+const addVat3=addTaxRate2(0.23);
+console.log(addVat3(200));
+
+
+
+// simple functions
+const greet = function(greeting) {
+    return function(name){
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+const greet2 =(greeting) => { return (name) => console.log(`${greeting} ${name}`)};
 const greetHey = greet (`hey`)
 greetHey('szymi');
 greetHey('sob');
@@ -86,30 +146,30 @@ const szymi = {
 }
 
 
-const checkIn = function(flightNum,passenger){
+// const checkIn = function(flightNum,passenger){
 
-    // flightNum.fc='LM';
-    // flightNum.fn='999';
-    flightNum='LH345';
-    passenger.name = 'MR.'+ passenger.name;
-    if(passenger.passport === 2121232323){
-        alert('check in')
-    }
-    else {
-        alert('wrong passport')
-    }
-}
+//     // flightNum.fc='LM';
+//     // flightNum.fn='999';
+//     flightNum='LH345';
+//     passenger.name = 'MR.'+ passenger.name;
+//     if(passenger.passport === 2121232323){
+//         alert('check in')
+//     }
+//     else {
+//         alert('wrong passport')
+//     }
+// }
 
-checkIn(flight,szymi);
-console.log(flight2);
-console.log(szymi);
+// checkIn(flight,szymi);
+// console.log(flight2);
+// console.log(szymi);
 
-const newPassport = function(person){
-    person.passport=Math.trunc(Math.random()*100000000);
-}
-newPassport(szymi);
-console.log(szymi);
-checkIn(flight,szymi);
+// const newPassport = function(person){
+//     person.passport=Math.trunc(Math.random()*100000000);
+// }
+// newPassport(szymi);
+// console.log(szymi);
+// checkIn(flight,szymi);
 
 
 
