89b2035f3b8f51408f2cb147b6aaaa17e150f415
szymeck <szymek.sobierajski@gmail.com>
2024-01-03 23:24:24 +0100

    asd

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -80,5 +80,7 @@ console.log(arr.slice(2));
 console.log(arr.slice(2,4));
 console.log(arr.slice(-2));
 console.log(arr.slice(1));
+console.log(arr.slice(1));
+console.log(arr.slice(-1));
 console.log(arr.slice(2,-2));
 console.log(arr.slice(2,-2));
